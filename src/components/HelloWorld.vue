6fa7c95829a8efc8547209c5855474369497a1ba
DESKTOP-P9EK4AV\Tobias Katra <tobias.katra@consid.se>
2023-02-13 15:00:30 +0100

    Installed sass and broke styling out of the vue files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+  defineProps<{
+    msg: string
+  }>()
 </script>
+
+<style src="./HelloWorld.scss" lang="scss"></style>
 
 <template>
   <div class="greetings">
@@ -14,27 +16,3 @@ defineProps<{
     </h3>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,7 @@
+<style src="./AboutView.scss" lang="scss"></style>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
